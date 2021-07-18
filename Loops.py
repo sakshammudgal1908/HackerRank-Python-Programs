04928fb962f7cdc63a4e2ee62c6fe507682e2c39
sakshammudgal1908 <84904254+sakshammudgal1908@users.noreply.github.com>
2021-07-18 21:31:43 +0530

    Create Loops.py

---
+++ Loops.py
@@ -0,0 +1,24 @@
+Sample Input 0
+
+5
+Sample Output 0
+
+0
+1
+4
+9
+16
+
+
+#using while loop
+a=int(input())
+i=0
+while i<a:
+    print(i*i)
+    i=i+1
+    
+    
+ #using for loop
+a=int(input())
+for i in range(a):
+    print(i*i)
