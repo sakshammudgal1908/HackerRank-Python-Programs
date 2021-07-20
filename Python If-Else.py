4c2237eec9bed022f0a21448bdc9271f94dc3875
sakshammudgal1908 <84904254+sakshammudgal1908@users.noreply.github.com>
2021-07-20 21:38:01 +0530

    Create Python If-Else.py

---
+++ Python If-Else.py
@@ -0,0 +1,16 @@
+Sample Input
+a=int(input())
+if(a%2!=0):
+    print("Weird")
+elif(2<=a<=5):
+    print("Not Weird")
+elif(6<=a<=20):
+    print("Weird")
+elif(a>20):
+    print("Not Weird")
+
+Sample output
+4, Not Weird
+
+    
+    
