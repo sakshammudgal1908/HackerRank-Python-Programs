ef12a64d59c6772a9cbed962165d29d7612b3b8c
sakshammudgal1908 <84904254+sakshammudgal1908@users.noreply.github.com>
2021-08-09 19:42:02 +0530

    Create List Comprehensions.py

---
+++ List Comprehensions.py
@@ -0,0 +1,17 @@
+'''Sample Input 0
+
+1
+1
+1
+2
+Sample Output 0
+
+[[0, 0, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0], [1, 1, 1]]'''
+
+if __name__ == '__main__':
+    x = int(raw_input())
+    y = int(raw_input())
+    z = int(raw_input())
+    n = int(raw_input())
+    
+    print([[a,b,c] for a in range(x+1) for b in range(y+1) for c in range(z+1) if a+b+c!=n])
